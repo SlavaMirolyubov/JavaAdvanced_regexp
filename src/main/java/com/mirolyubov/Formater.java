9b07ca726700bb1c75d970e23074c06ed83a6d7c
Slava <grandwhiskey@yandex.ru>
2019-05-08 14:17:18 +0400

    commit

---
+++ src/main/java/com/mirolyubov/Formater.java
@@ -0,0 +1,5 @@
+package com.mirolyubov;
+
+public class Formater {
+
+}
---
+++ src/main/java/com/mirolyubov/Main.java
@@ -0,0 +1,8 @@
+package com.mirolyubov;
+
+public class Main {
+    public static void main(String[] args) {
+        String fileOriginal = "./src/main/resources/originalFile.txt";
+        String fileModified = "./src/main/resources/modifiedFile.txt";
+    }
+}

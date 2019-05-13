25e1e895bb1b5a8ba68d30e92c2a7d3fd9cd273f
Slava <grandwhiskey@yandex.ru>
2019-05-13 11:29:30 +0400

    commit

--- src/main/java/com/mirolyubov/Formater.java
+++ src/main/java/com/mirolyubov/Formater.java
@@ -1,5 +1,23 @@
 package com.mirolyubov;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Formater {
 
+    public static String formatNumbers(String string) {
+        String badNumberStroke = string;
+        Pattern pattern = Pattern.compile("\\+\\d\\(\\d{3}\\)\\s\\d{3}\\s\\d{2}\\s\\d{2}");
+        Matcher matcher = pattern.matcher(string);
+
+        while (matcher.find()) {
+            String number = matcher.group();
+            String addPrefix = String.valueOf(Integer.valueOf(number.substring(1,2)) + 1);
+            String finalNumber = number.replaceAll("\\+\\d", addPrefix)
+                    .replaceAll("\\W", "");
+            badNumberStroke = badNumberStroke.replace(number, finalNumber);
+        }
+
+        return badNumberStroke;
+    }
 }
--- src/main/java/com/mirolyubov/Main.java
+++ src/main/java/com/mirolyubov/Main.java
@@ -1,8 +1,25 @@
 package com.mirolyubov;
 
+import org.apache.log4j.Logger;
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        String fileOriginal = "./src/main/resources/originalFile.txt";
-        String fileModified = "./src/main/resources/modifiedFile.txt";
+
+    private static Logger logger = Logger.getLogger(Main.class);
+
+    public static void main(String[] args) throws IOException {
+        String temporaryString;
+        String fileOriginal = "./src/main/resources/original.txt";
+        String fileModified = "./src/main/resources/modified.txt";
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileOriginal));
+             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileModified))) {
+            while ((temporaryString = bufferedReader.readLine()) != null) {
+                bufferedWriter.write(Formater.formatNumbers(temporaryString));
+                bufferedWriter.newLine();
+            }
+        } catch (FileNotFoundException e) {
+            logger.error("File not found!");
+        }
     }
 }
